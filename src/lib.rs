ef36868f05dd045a990b78800ebe8571e5bfb573
Alyssa Evans <alyssa.evans@pluto.tv>
2024-12-23 14:07:57 -0800

    miette, solve parts

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,41 @@
 pub trait Solution {
     /// Ensures the output can be converted to a string
-    type Output: std::fmt::Display;  
+    type Output: std::fmt::Display + Default;  
+
+    /// Required for AoC
+    fn part1(&mut self) -> miette::Result<Self::Output> {
+        todo!()
+    }
+
+    /// Required for AoC
+    fn part2(&mut self) -> miette::Result<Self::Output> {
+        todo!()
+    }
+
+    /// Optional, for everybody.codes or bonus AoC
+    fn part3(&mut self) -> miette::Result<Self::Output> {
+        // Ok("".to_string())
+        Ok(<Self as Solution>::Output::default())
+    }
+
+    fn solve(&mut self, which: Part) -> miette::Result<String> {
+        Ok(match which {
+            Part::One => self.part1()?.to_string(),
+            Part::Two => self.part2()?.to_string(),
+            Part::Three => self.part3()?.to_string(),
+        })
+    }
+}
+
+/// Puzzla parts
+#[derive(Debug, Clone, Copy)]
+pub enum Part {
+    /// AoC
+    One,
+    /// AoC
+    Two,
+    /// for everybody.codes
+    Three,
 }
 
 #[cfg(test)]
@@ -9,19 +44,42 @@ mod tests {
 
     struct Day;
 
-    impl Day {
-        fn solve() -> <Self as Solution>::Output {
-            "Hello, Christmas!".to_string()
+    impl Solution for Day {
+        type Output = String;
+
+        fn part1(&mut self) -> miette::Result<Self::Output> {
+            Ok("Hello, Rudolph!".into())
+        }
+
+        fn part2(&mut self) -> miette::Result<Self::Output> {
+            Ok("Hello, Santa!".into())
         }
     }
 
-    impl Solution for Day {
-        type Output = String;
+    #[test]
+    fn test_part_1() -> miette::Result<()> {
+        let mut day = Day;
+
+        let solution = day.solve(Part::One)?;
+        assert_eq!("Hello, Rudolph!".to_string(), solution);
+        Ok(())
     }
 
     #[test]
-    fn it_works() {
-        let solution = Day::solve().to_string();
-        assert_eq!("Hello, Christmas!".to_string(), solution);
+    fn test_part_2() -> miette::Result<()> {
+        let mut day = Day;
+
+        let solution = day.solve(Part::Two)?;
+        assert_eq!("Hello, Santa!".to_string(), solution);
+        Ok(())
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_part_3() {
+        let mut day = Day;
+
+        let solution = day.solve(Part::Three).unwrap();
+        assert_eq!("Hello, Dasher!".to_string(), solution);
     }
 }
