745839a7113f9c2e188611b9ac9846c2e56decb2
Alyssa Evans <alyssa.evans@pluto.tv>
2024-12-24 14:05:00 -0800

    2015-12-01 - Part 2

--- examples/2015-12-01.rs
+++ examples/2015-12-01.rs
@@ -2,10 +2,10 @@
 
 use aoc_ornaments::{Part, Solution};
 
-struct Day(i32);
+struct Day(Vec<i32>);
 
 impl std::ops::Deref for Day {
-    type Target = i32;
+    type Target = Vec<i32>;
 
     fn deref(&self) -> &Self::Target {
         &self.0
@@ -16,7 +16,27 @@ impl Solution for Day {
     type Output = i32;
 
     fn part1(&mut self) -> miette::Result<Self::Output> {
-        Ok(**self)
+        let output = self.iter().sum();
+        Ok(output)
+    }
+
+    fn part2(&mut self) -> miette::Result<Self::Output> {
+        // seems to be 1-indexed, not 0-indexed
+        // let mut idx = 1;
+
+        let output = self.iter()
+            .enumerate()
+            .scan(1, |state, (_i, x)| {
+                *state += x;
+            if *state < 1 {
+                // idx += i;
+                None  // Stops iteration here
+            } else {
+                Some(*state)
+            }
+        }).count();
+
+        Ok((output + 1) as i32)
     }
 }
 
@@ -30,7 +50,7 @@ impl TryFrom<&str> for Day {
                 ')' => -1,
                 _ => 0,
             }
-        }).sum();
+        }).collect();
 
         Ok(Self(parsed))
     }
@@ -39,10 +59,10 @@ impl TryFrom<&str> for Day {
 fn main() -> miette::Result<()> {
     let mut day = Day::try_from(include_str!("./inputs/2015-12-01.txt"))?;
     let part1 = day.solve(Part::One)?;
-    // let part2 = day.solve(Part::Two)?;
+    let part2 = day.solve(Part::Two)?;
 
     println!("Part 1: {}", part1);
-    // println!("Part 2: {}", part2);
+    println!("Part 2: {}", part2);
 
     Ok(())
 }
@@ -63,8 +83,16 @@ mod tests {
     #[case("))(", -1)]
     #[case(")))", -3)]
     #[case(")())())", -3)]
-    fn test_cases(#[case] input: &str, #[case] expected: i32) {
+    fn test_cases_part1(#[case] input: &str, #[case] expected: i32) {
         let mut day = Day::try_from(input).unwrap();
         assert_eq!(day.solve(Part::One).unwrap(), expected.to_string());
+    }
+
+    #[rstest]
+    #[case(")", 1)]
+    #[case("()())", 5)]
+    fn test_cases_part2(#[case] input: &str, #[case] expected: i32) {
+        let mut day = Day::try_from(input).unwrap();
+        assert_eq!(day.solve(Part::Two).unwrap(), expected.to_string());
     }
 }
