0a33bc44326b2593b25bcab7f3bbcd84b013f337
Alyssa Evans <alyssa.evans@pluto.tv>
2024-12-26 21:18:18 -0800

    2015-12-04 Part 2

--- examples/2015-12-04.rs
+++ examples/2015-12-04.rs
@@ -24,11 +24,6 @@ impl FromStr for Day {
 }
 
 impl Day {
-    // fn generate_hash(key: &str, number: u32) -> String {
-    //     let input = format!("{}{}", key, number);
-    //     format!("{:x}", md5::compute(input))
-    // }
-
     fn generate_hash_bytes(key: &str, number: u32) -> [u8; 16] {
         let input = format!("{}{}", key, number);
         md5::compute(input).0
@@ -38,19 +33,25 @@ impl Day {
         // First two bytes must be 0
         hash[0] == 0 && hash[1] == 0 && (hash[2] & 0xf0) == 0
     }
+    
+    fn has_six_leading_zeros(hash: &[u8; 16]) -> bool {
+        // First three bytes must be 0
+        hash[0] == 0 && hash[1] == 0 && hash[2] == 0
+    }
 }
 
 impl Solution for Day {
     type Output = u32;
 
     fn part1(&mut self) -> miette::Result<Self::Output> {
-        // dbg!(&self);
-
-        // dbg!(Day::generate_hash(&self.0, 0));
-        // dbg!(Day::generate_hash_bytes(&self.0, 0));
-
         (1..).find(|&n| {
             Day::has_five_leading_zeros(&Day::generate_hash_bytes(&self.0, n))
+        }).ok_or_else(|| miette::miette!("No solution found"))
+    }
+
+    fn part2(&mut self) -> miette::Result<Self::Output> {
+        (1..).find(|&n| {
+            Day::has_six_leading_zeros(&Day::generate_hash_bytes(&self.0, n))
         }).ok_or_else(|| miette::miette!("No solution found"))
     }
 }
@@ -58,10 +59,10 @@ impl Solution for Day {
 fn main() -> miette::Result<()> {
     let mut day = Day::from_str(include_str!("./inputs/2015-12-04.txt"))?;
     let part1 = day.solve(Part::One)?;
-    // let part2 = day.solve(Part::Two)?;
+    let part2 = day.solve(Part::Two)?;
 
     println!("Part 1: {}", part1);
-    // println!("Part 2: {}", part2);
+    println!("Part 2: {}", part2);
 
     Ok(())
 }
