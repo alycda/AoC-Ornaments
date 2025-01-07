dea8b5044dbda012bf0491eaffbd4213d3169c47
Alyssa Evans <alyssa.evans@pluto.tv>
2025-01-06 18:27:15 -0800

    2015 Day 24 Part 2

--- examples/2015-12-24.rs
+++ examples/2015-12-24.rs
@@ -26,99 +26,9 @@ impl FromStr for Day {
 }
 
 impl Day {
-    // fn find_combinations_with_sum(numbers: &[usize], target: usize, size: usize) -> Vec<Vec<usize>> {
-    //     numbers
-    //         .iter()
-    //         .combinations(size)
-    //         .filter(|combo| combo.iter().copied().sum::<usize>() == target)
-    //         .map(|combo| combo.into_iter().copied().collect())
-    //         .collect()
-    // }
-
-    fn find_all_combinations_with_sum(numbers: &[usize], target: usize) -> Vec<Vec<usize>> {
-        (1..=numbers.len())
-            .flat_map(|size| {
-                numbers
-                    .iter()
-                    .combinations(size)
-                    .filter(|combo| combo.iter().copied().sum::<usize>() == target)
-                    .map(|combo| combo.into_iter().copied().collect::<Vec<_>>())
-                    .collect::<Vec<_>>()
-            })
-            .collect()
-    }
-    
-    // If you need the product of each combination:
-    fn get_products(combinations: &[Vec<usize>]) -> Vec<usize> {
-        combinations
-            .iter()
-            .map(|combo| combo.iter().product())
-            .collect()
-    }
-
-    fn find_best_combination(numbers: &[usize], target: usize) -> Option<(Vec<usize>, usize)> {
-        let mut best: Option<(Vec<usize>, usize)> = None;
-        
-        // Start with smallest combinations first
-        for size in 1..=numbers.len() {
-            // If we already found a smaller combination, we can stop
-            if let Some((best_combo, _)) = &best {
-                if size > best_combo.len() {
-                    break;
-                }
-            }
-            
-            for combo in numbers.iter().combinations(size) {
-                let sum: usize = combo.iter().copied().sum();
-                if sum == target {
-                    let combo_vec: Vec<usize> = combo.into_iter().copied().collect();
-                    let product: usize = combo_vec.iter().product();
-                    
-                    match &best {
-                        None => best = Some((combo_vec, product)),
-                        Some((best_combo, best_product)) => {
-                            if combo_vec.len() < best_combo.len() || 
-                               (combo_vec.len() == best_combo.len() && product > *best_product) {
-                                best = Some((combo_vec, product));
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        
-        best
-    }
-}
-
-impl Solution for Day {
-    type Output = usize;
-
-    // for n in range(1,len(weights)):
-    // good = [x for x in list(combinations(weights,n)) if sum(x) == sum(weights)/3]
-    // if len(good) > 0:
-    //     break
-
-    // smallest = min(good, key=lambda x: reduce(mul, x))
-    // print reduce(mul, smallest)
-
-    fn part1(&mut self) -> aoc_ornaments::SolutionResult<<Self as Solution>::Output> {
-        let total_weight = dbg!(self.iter().sum::<usize>());
-        let group_weight = total_weight / 3;
-        // let combinations = Day::find_combinations_with_sum(self, group_weight, 3);
-
-        // let combinations = Self::find_all_combinations_with_sum(&self, group_weight);
-        // // println!("Combinations that sum to {}: {:?}", group_weight, combinations);
-        // println!("Number of combinations: {}", combinations.len());
-        
-        // let products = Self::get_products(&combinations);
-        // println!("Products of combinations: {:?}", products);
-
-        // let a = Self::find_best_combination(&self.iter().copied().collect::<Vec<_>>(), group_weight)
-        //     .map(|(_, product)| product as usize);
-        //     // .ok_or_else(|| "No valid combination found".into())
-
-        // Ok(a.unwrap())
+    fn quantum_entanglement(&self, compartments: usize) -> Result<usize, miette::Error> {
+        let total_weight = self.iter().sum::<usize>();
+        let group_weight = total_weight / compartments;
 
         // Find first group of valid combinations
         let mut good_combinations = Vec::new();
@@ -139,22 +49,33 @@ impl Solution for Day {
             .min_by_key(|combo| combo.iter().product::<usize>()) 
         {
             let product: usize = smallest.iter().product();
-            println!("Smallest product: {}", product);
 
             return Ok(product)
         }
 
-        todo!()
+        Err(miette::miette!("No valid combinations found"))
+    }
+}
+
+impl Solution for Day {
+    type Output = usize;
+
+    fn part1(&mut self) -> aoc_ornaments::SolutionResult<<Self as Solution>::Output> {
+        self.quantum_entanglement(3)
+    }
+
+    fn part2(&mut self) -> aoc_ornaments::SolutionResult<<Self as Solution>::Output> {
+        self.quantum_entanglement(4)
     }
 }
 
 fn main() -> miette::Result<()> {
     let mut day = Day::from_str(include_str!("./inputs/2015-12-24.txt"))?;
     let part1 = day.solve(Part::One)?;
-    // let part2 = day.solve(Part::Two)?;
+    let part2 = day.solve(Part::Two)?;
 
-    println!("Part 1: {}", part1); // < 344266178323
-    // println!("Part 2: {}", part2);
+    println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2);
 
     Ok(())
 }
