46ada2bce620556b4910dd70e6309e88d3f6f6da
Alyssa Evans <alyssa.evans@pluto.tv>
2025-01-07 21:48:29 -0800

    cleanup

--- examples/2015-12-03.rs
+++ examples/2015-12-03.rs
@@ -1,58 +1,23 @@
 //! Day 3: Perfectly Spherical Houses in a Vacuum
 
-use std::{collections::HashSet, str::FromStr};
+use std::{marker::PhantomData, str::FromStr};
 
-use aoc_ornaments::{spatial::{Direction, Position, Visited}, Part, Solution};
-
-#[derive(Debug, derive_more::Deref)]
-pub struct Part1(Visited<usize>);
+use aoc_ornaments::{spatial::{Direction, Position, UniquePositions}, Part, Solution};
 
 #[derive(Debug)]
-pub struct Part2 {
-    santa: Visited<usize>,
-    robot: Visited<usize>,
-}
+pub struct Day<P>(UniquePositions, PhantomData<P>);
 
 #[derive(Debug)]
-struct Day<P>(P);
-// struct Day(Hashset<Position>, PhantomData<P>);
+pub struct Part1;
 
-impl Solution for Day<Part1> {
-    type Output = usize;
+#[derive(Debug)]
+pub struct Part2;
 
-    fn part1(&mut self) -> aoc_ornaments::SolutionResult<Self::Output> {
-        Ok(self.keys().count())
-    }
+impl<P> std::ops::Deref for Day<P> {
+    type Target = UniquePositions;
 
-    fn part2(&mut self) -> aoc_ornaments::SolutionResult<Self::Output> {
-        unimplemented!("Part 2")
-    }
-}
-
-impl Solution for Day<Part2> {
-    type Output = usize;
-
-    fn part1(&mut self) -> aoc_ornaments::SolutionResult<Self::Output> {
-        unimplemented!("Part 1")
-    }
-
-    fn part2(&mut self) -> aoc_ornaments::SolutionResult<Self::Output> {
-        let mut common = HashSet::new();
-
-        // dbg!(&self.0.santa);
-        // dbg!(&self.0.robot);
-
-        // panic!("halt");
-
-        self.0.santa.keys().for_each(|k| {
-            if self.0.robot.contains_key(k) {
-                common.insert(k);
-            }
-        });
-
-        // Ok(uncommon.len())
-
-        Ok(self.0.santa.keys().len() + self.0.robot.keys().len() - common.len() * 2)
+    fn deref(&self) -> &Self::Target {
+        &self.0
     }
 }
 
@@ -60,17 +25,17 @@ impl FromStr for Day<Part1> {
     type Err = miette::Error;
 
     fn from_str(input: &str) -> miette::Result<Self> {
-        let mut visited = Visited::default(); //Visited::with_start(Position::ZERO);
+        let mut visited = UniquePositions::new();
         let mut position = Position::ZERO;
 
-        visited.insert(position, 1);
+        visited.insert(position);
 
         for c in input.chars() {
             position += Direction::parse(c)?.to_offset();
-            *visited.entry(position).or_insert(0) += 1;
+            visited.insert(position);
         }
 
-        Ok(Self(Part1(visited)))
+        Ok(Self(visited, PhantomData))
     }
 }
 
@@ -78,28 +43,45 @@ impl FromStr for Day<Part2> {
     type Err = miette::Error;
 
     fn from_str(input: &str) -> miette::Result<Self> {
-        let mut santa = Visited::default(); //Visited::with_start(Position::ZERO);
-        let mut robot = Visited::default(); //Visited::with_start(Position::ZERO);
+        let mut santa_visited = UniquePositions::new();
+        let mut robot_visited = UniquePositions::new();
         let mut santa_position = Position::ZERO;
         let mut robot_position = Position::ZERO;
 
-        santa.insert(santa_position, 1);
-        robot.insert(robot_position, 1);
-        
-        input.chars().enumerate().for_each(|(i, c)| {
-            if i % 2 == 0 {
-                santa_position += Direction::parse(c).unwrap().to_offset();
-                *santa.entry(santa_position).or_insert(0) += 1;
-            } else {
-                robot_position += Direction::parse(c).unwrap().to_offset();
-                *robot.entry(robot_position).or_insert(0) += 1;
-            }
-        });
+        santa_visited.insert(santa_position);
+        robot_visited.insert(robot_position);
 
-        Ok(Self(Part2{ santa, robot }))
+        input.chars()
+            .enumerate()
+            .for_each(|(i, c)| {
+                if i % 2 == 0 {
+                    santa_position += Direction::parse(c).expect("valid char").to_offset();
+                    santa_visited.insert(santa_position);
+                } else {
+                    robot_position += Direction::parse(c).expect("valid char").to_offset();
+                    robot_visited.insert(robot_position);
+                }
+            });
+
+        Ok(Self(santa_visited.union(&robot_visited).copied().collect(), PhantomData))
     }
 }
 
+impl<P> Solution for Day<P> where Day<P>: FromStr {
+    type Output = usize;
+
+    /// Count the number of houses visited by Santa.
+    fn part1(&mut self) -> aoc_ornaments::SolutionResult<<Self as Solution>::Output> {
+        Ok(self.len())
+    }
+
+    /// Count the number of houses visited by Santa and the robot.
+    fn part2(&mut self) -> aoc_ornaments::SolutionResult<<Self as Solution>::Output> {
+        Ok(self.len())
+    }
+}
+
+/// Run Part 1 and Part 2.
 fn main() -> miette::Result<()> {
     let input = include_str!("./inputs/2015-12-03.txt");
     let mut day_part1 = Day::<Part1>::from_str(input)?;
@@ -108,11 +90,10 @@ fn main() -> miette::Result<()> {
     let part2 = day_part2.solve(Part::Two)?;
 
     println!("Part 1: {}", part1);
-    println!("Part 2: {}", part2); // > 2440
+    println!("Part 2: {}", part2);
 
     Ok(())
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -133,6 +114,6 @@ mod tests {
     #[case("^>v<", 3)]
     #[case("^v^v^v^v^v", 11)]
     fn test_cases_part2(#[case] input: &str, #[case] expected: u32) {
-        assert_eq!(Day::<Part2>::from_str(input).unwrap().solve(Part::Two).unwrap(), (expected - 1).to_string());
+        assert_eq!(Day::<Part2>::from_str(input).unwrap().solve(Part::Two).unwrap(), expected.to_string());
     }
 }
