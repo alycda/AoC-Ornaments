e913b461dd753f17bc3a896e6509733e8af6fd60
Alyssa Evans <alyssa.evans@pluto.tv>
2025-01-21 20:52:19 -0800

    Traveling Sales WIP

--- examples/2015-12-09.rs
+++ examples/2015-12-09.rs
@@ -1,6 +1,6 @@
 //! Day 9: All in a Single Night
 
-use std::{collections::HashSet, marker::PhantomData, str::FromStr};
+use std::{collections::HashSet, str::FromStr};
 
 use aoc_ornaments::{linear::Distances, Part, Solution};
 
@@ -23,7 +23,7 @@ impl<T: std::ops::Add<Output = T> + Clone + Copy + Ord + Default> TSP<T> {
             let mut tsp = Self::new(strategy);
             tsp.call(map, start, &mut remaining, T::default());
 
-            // Update overall minimum if this path is shorter
+            // Update overall best if this path is STRATEGY
             if let Some(path_length) = *tsp {
                 best_path = match best_path {
                     None => Some(path_length),
@@ -77,70 +77,55 @@ impl<T> std::ops::Deref for TSP<T> {
     }
 }
 
-#[derive(Debug)]
-struct Day<P>(Distances<u32>, PhantomData<P>);
+#[derive(Debug, derive_more::Deref)]
+struct Day(Distances<u32>);
 
-trait Strategy {
-    const COMPARE: fn(u32, u32) -> u32;
-}
-
-impl<P> std::ops::Deref for Day<P> {
-    type Target = Distances<u32>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-/// Travelling Salesman Problem
-struct ShortestPath;
-
-/// Travelling Salesman Problem
-struct LongestPath;
-
-impl Strategy for ShortestPath {
-    const COMPARE: fn(u32, u32) -> u32 = u32::min;
-}
-
-impl Strategy for LongestPath {
-    const COMPARE: fn(u32, u32) -> u32 = u32::max;
-}
-
-impl<P> FromStr for Day<P> {
+impl FromStr for Day {
     type Err = miette::Error;
 
     fn from_str(input: &str) -> miette::Result<Self> {
-        Ok(Self(
-            input.lines().fold(Distances::new(), |mut map, line| {
+        Ok(Self(input.lines().fold(
+            Distances::new(),
+            |mut map, line| {
                 let parts: Vec<&str> = line.split(' ').collect();
 
                 match parts.as_slice() {
                     [a, "to", b, "=", d] => {
                         let distance = d.parse().unwrap();
                         map.insert_ordered(a.to_string(), b.to_string(), distance);
+                        // Also insert the reverse direction with the same distance
+                        // map.insert_ordered(b.to_string(), a.to_string(), distance);
                     }
                     _ => panic!("Invalid input"),
                 }
 
                 map
-            }),
-            PhantomData,
-        ))
+            },
+        )))
     }
 }
 
-impl<P: Strategy> Solution for Day<P> {
+impl Solution for Day {
     type Output = u32;
 
-    fn solve(&mut self, _part: Part) -> aoc_ornaments::SolutionResult<String> {
-        Ok(TSP::<u32>::best(self, P::COMPARE).unwrap().to_string())
+    fn solve(&mut self, part: Part) -> aoc_ornaments::SolutionResult<String> {
+        let strategy = match part {
+            Part::One => Self::Output::min,
+            Part::Two => Self::Output::max,
+            // _ => u32::eq,
+            _ => unimplemented!(),
+        };
+
+        Ok(TSP::<Self::Output>::best(self, strategy)
+            .unwrap()
+            .to_string())
     }
 }
 
 fn main() -> miette::Result<()> {
     let input = include_str!("./inputs/2015-12-09.txt");
-    let part1 = Day::<ShortestPath>::from_str(input)?.solve(Part::One)?;
-    let part2 = Day::<LongestPath>::from_str(input)?.solve(Part::Two)?;
+    let part1 = Day::from_str(input)?.solve(Part::One)?;
+    let part2 = Day::from_str(input)?.solve(Part::Two)?;
 
     println!("Part 1: {}", part1);
     println!("Part 2: {}", part2);
@@ -155,13 +140,10 @@ mod tests {
     #[test]
     fn test_part1() {
         let input = "London to Dublin = 464
-Dublin to London = 464
 London to Belfast = 518
-Belfast to London = 518
-Dublin to Belfast = 141
-Belfast to Dublin = 141";
+Dublin to Belfast = 141";
 
-        let mut day = Day::<ShortestPath>::from_str(input).unwrap();
+        let mut day = Day::from_str(input).unwrap();
         let result = day.solve(Part::One).unwrap();
 
         assert_eq!(result, "605");
@@ -170,13 +152,10 @@ Belfast to Dublin = 141";
     #[test]
     fn test_part2() {
         let input = "London to Dublin = 464
-Dublin to London = 464
 London to Belfast = 518
-Belfast to London = 518
-Dublin to Belfast = 141
-Belfast to Dublin = 141";
+Dublin to Belfast = 141";
 
-        let mut day = Day::<LongestPath>::from_str(input).unwrap();
+        let mut day = Day::from_str(input).unwrap();
         let result = day.solve(Part::Two).unwrap();
 
         assert_eq!(result, "982");
