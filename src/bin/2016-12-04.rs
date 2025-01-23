51293c76f4e7124c0da90f1c9ad942c9560681d0
Alyssa Evans <alyssa.evans@pluto.tv>
2025-01-22 16:50:34 -0800

    2016 Day 4 Part 1

--- src/bin/2016-12-04.rs
+++ src/bin/2016-12-04.rs
@@ -1,13 +1,12 @@
 //! # Day 4: Security Through Obscurity
 //!
 
-use std::{collections::BTreeMap, str::FromStr};
+use std::{collections::HashMap, str::FromStr};
 
 use aoc_ornaments::{Part, Solution};
 use nom::{
-    bytes::complete::{take_until, take_while},
     character::complete::{alpha1, char, digit1},
-    multi::{many0, many1},
+    multi::many1,
     sequence::{delimited, terminated, tuple},
     IResult,
 };
@@ -16,7 +15,7 @@ use nom::{
 /// - room number
 /// - checksum
 #[derive(Debug, derive_more::Deref)]
-struct Day(Vec<(usize, String, BTreeMap<char, usize>)>);
+struct Day(Vec<(usize, String, HashMap<char, usize>)>);
 
 impl FromStr for Day {
     type Err = miette::Error;
@@ -32,31 +31,10 @@ impl FromStr for Day {
 }
 
 impl Day {
-    fn parse_line(input: &str) -> IResult<&str, (usize, String, BTreeMap<char, usize>)> {
-        // let (a, b) = tuple((take_until("-"), digit1))(input)?;
-        // let (a, b) = tuple((
-        //     take_while::<_, _, _>(tuple((alpha1, char('-')))),
-        //     char('-'),
-        //     digit1,
-        // ))?;
-
+    fn parse_line(input: &str) -> IResult<&str, (usize, String, HashMap<char, usize>)> {
         let (input, parts) = many1(terminated(alpha1, char('-')))(input)?;
         let map = Self::count_frequencies(&parts);
         let (_, (id, checksum)) = Self::parse_id_and_checksum(input)?;
-
-        dbg!(&id, &checksum, &map);
-
-        // let parts: Vec<_> = input.split('-').collect();
-
-        // dbg!(&parts);
-
-        // match parts.as_slice() {
-        //     [a, b, c, d, e, f] => {
-        //         dbg!(a, b, c, d, e);
-        //         let s = format!("{a}-{b}-{c}-{d}")
-        //     }
-        //     _ => panic!("Invalid input"),
-        // }
 
         Ok((
             "",
@@ -68,8 +46,8 @@ impl Day {
         tuple((digit1, delimited(char('['), alpha1, char(']'))))(input)
     }
 
-    fn count_frequencies(items: &[&str]) -> BTreeMap<char, usize> {
-        let mut freq_map: BTreeMap<char, usize> = BTreeMap::new();
+    fn count_frequencies(items: &[&str]) -> HashMap<char, usize> {
+        let mut freq_map: HashMap<char, usize> = HashMap::new();
 
         for &item in items {
             for c in item.chars() {
@@ -90,8 +68,19 @@ impl Solution for Day {
         Ok(self
             .iter()
             .filter_map(|(id, checksum, map)| {
-                let a = map.iter().map(|(k, v)| k.to_string()).collect::<String>();
+                // let a = map.iter().map(|(k, v)| k.to_string()).collect::<String>();
                 // dbg!(map.iter().collect::<String>());
+
+                // Convert to vec for sorting
+                let mut freq_vec: Vec<_> = map.iter().collect();
+
+                // Sort by value (count) descending, then by key for stable sorting
+                freq_vec.sort_by(|a, b| b.1.cmp(a.1).then(a.0.cmp(b.0)));
+
+                let a = freq_vec
+                    .iter()
+                    .map(|(k, v)| k.to_string())
+                    .collect::<String>();
 
                 // if a == *checksum {
                 if a.starts_with(checksum) {
