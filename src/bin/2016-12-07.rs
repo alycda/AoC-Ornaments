97c4769e6a6896ff1b9287458d81d47e68eb7ed5
Alyssa Evans <alyssa.evans@pluto.tv>
2025-01-24 22:22:45 -0800

    2016 Day 7 Part 1

--- src/bin/2016-12-07.rs
+++ src/bin/2016-12-07.rs
@@ -5,16 +5,12 @@ use std::str::FromStr;
 
 use aoc_ornaments::{Part, Solution};
 use nom::{
-    branch::alt,
-    bytes::complete::tag,
-    character::complete::{alpha1, char},
-    multi::{many0, many1, separated_list1},
-    sequence::{delimited, tuple},
-    IResult,
+    branch::alt, bytes::complete::tag, character::complete::alpha1, multi::many1,
+    sequence::delimited, IResult,
 };
 
 #[derive(Debug, derive_more::Deref)]
-struct Day(Vec<String>);
+struct Day(Vec<Vec<String>>);
 
 impl FromStr for Day {
     type Err = miette::Error;
@@ -22,46 +18,27 @@ impl FromStr for Day {
     fn from_str(input: &str) -> Result<Self, Self::Err> {
         Ok(Day(input
             .lines()
-            // .map(String::from)
             .map(|line| {
-                // let out = Self::parse_line(line).unwrap();
+                let (_, sequences) = Self::parse_line(line).unwrap();
 
-                // dbg!(&out);
-
-                // let (input, (sequences, hypernet)) = tuple((
-                //     separated_list1(delimited(char('['), alpha1, char(']')), alpha1),
-                //     many0(delimited(char('['), alpha1, char(']'))),
-                // ))(input)
-                // .unwrap();
-                // dbg!(input, sequences, hypernet);
-
-                dbg!(Self::parse_line(line).unwrap());
-
-                "".to_string()
+                sequences.iter().map(|x| String::from(*x)).collect()
             })
-            .collect::<Vec<String>>()))
+            .collect::<Vec<_>>()))
     }
 }
 
 impl Day {
-    // sequences, hypernet sequences
-    // fn parse_line(input: &str) -> IResult<&str, (Vec<String>, Vec<String>)> {
-    //     // let (a, b) = alt((alpha1, delimited(char('['), alpha1, char('['))))(input)?;
-    //     let (a, b) = many1(alt((alpha1, delimited(char('['), alpha1, char('[')))))(input)?;
-
-    //     dbg!(a, b);
-
-    //     todo!()
-    // }
-    // fn parse_line(input: &str) -> IResult<&str, (Vec<&str>, Vec<&str>)> {
-    //     // many1(alt((alpha1, delimited(char('['), alpha1, char('[')))))(input)
-    //     tuple((
-    //         separated_list1(delimited(char('['), alpha1, char(']')), alpha1),
-    //         many0(delimited(char('['), alpha1, char(']'))),
-    //     ))(input)
-    // }
+    /// sequences, hypernet sequences
     fn parse_line(input: &str) -> IResult<&str, Vec<&str>> {
         many1(alt((delimited(tag("["), alpha1, tag("]")), alpha1)))(input)
+    }
+
+    fn supports_tls(input: &str) -> bool {
+        input
+            .chars()
+            .collect::<Vec<_>>()
+            .windows(4)
+            .any(|x| x[0] == x[3] && x[0] != x[1] && x[1] == x[2])
     }
 }
 
@@ -69,8 +46,21 @@ impl Solution for Day {
     type Output = usize;
 
     fn part1(&mut self) -> aoc_ornaments::SolutionResult<Self::Output> {
-        // dbg!(&self);
-        todo!()
+        Ok(self
+            .iter()
+            .filter(|ipv7| {
+                let (left, right): (Vec<_>, Vec<_>) =
+                    ipv7.iter().enumerate().partition(|(idx, _)| idx % 2 == 0);
+
+                let sequence: Vec<_> = left.into_iter().map(|(_, s)| s).collect();
+                let hypernet: Vec<_> = right.into_iter().map(|(_, s)| s).collect();
+
+                let any_result = sequence.iter().any(|s| Day::supports_tls(s));
+                let all_result = hypernet.iter().all(|s| !Day::supports_tls(s));
+
+                any_result && all_result
+            })
+            .count())
     }
 }
 
@@ -84,4 +74,22 @@ fn main() -> miette::Result<()> {
     // println!("Part 2: {}", part2);
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use rstest::rstest;
+
+    #[rstest]
+    #[case("abba[mnop]qrst", true)]
+    #[case("abcd[bddb]xyyx", false)]
+    #[case("aaaa[qwer]tyui", false)]
+    #[case("ioxxoj[asdfgh]zxcvbn", true)]
+    fn test_cases(#[case] input: &str, #[case] expected: bool) {
+        let mut day = Day::from_str(input).unwrap();
+
+        assert_eq!(day.solve(Part::One).unwrap() == "1", expected);
+    }
 }
